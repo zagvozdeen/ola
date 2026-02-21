433e41c2dda4b5d49153fe4df2999a94ffc6981e
Denis Zagvozdin <zagvozdeen@gmail.com>
2026-02-21 18:24:09 +0500

    feat: update models and homepage template

--- internal/store/models/models.go
+++ internal/store/models/models.go
@@ -32,36 +32,82 @@ type File struct {
 }
 
 type Product struct {
-	ID        string    `json:"id"`
-	UserID    int       `json:"user_id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          string    `json:"id"`
+	UUID        string    `json:"uuid"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	PriceFrom   int       `json:"price_from"`
+	PriceTo     *int      `json:"price_to,omitempty"`
+	FileID      int       `json:"file_id"`
+	FileContent *string   `json:"file_content,omitempty"`
+	UserID      int       `json:"user_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type Service struct {
-	ID        string    `json:"id"`
-	UserID    int       `json:"user_id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          string    `json:"id"`
+	UUID        string    `json:"uuid"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	PriceFrom   int       `json:"price_from"`
+	PriceTo     *int      `json:"price_to,omitempty"`
+	FileID      int       `json:"file_id"`
+	FileContent *string   `json:"file_content,omitempty"`
+	UserID      int       `json:"user_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type Review struct {
-	ID        string    `json:"id"`
-	UserID    *int      `json:"user_id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          string    `json:"id"`
+	UUID        string    `json:"uuid"`
+	Name        string    `json:"name"`
+	Content     string    `json:"content"`
+	FileID      int       `json:"file_id"`
+	FileContent *string   `json:"file_content,omitempty"`
+	UserID      int       `json:"user_id"`
+	PublishedAt time.Time `json:"published_at"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type Order struct {
 	ID        string    `json:"id"`
-	UserID    int       `json:"user_id"`
+	UUID      string    `json:"uuid"`
+	Name      string    `json:"name"`
+	Phone     string    `json:"phone"`
+	Content   string    `json:"content"`
+	UserID    *int      `json:"user_id,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type Feedback struct {
 	ID        string    `json:"id"`
-	UserID    int       `json:"user_id"`
+	UUID      string    `json:"uuid"`
+	Name      string    `json:"name"`
+	Phone     string    `json:"phone"`
+	Content   string    `json:"content"`
+	UserID    *int      `json:"user_id,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Category struct {
+	ID        int       `json:"id"`
+	UUID      string    `json:"uuid"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type CategoryProduct struct {
+	CategoryID int `json:"category_id"`
+	ProductID  int `json:"product_id"`
+}
+
+type CategoryService struct {
+	CategoryID int `json:"category_id"`
+	ServiceID  int `json:"service_id"`
 }
