59fb692859f6cc9ba4e2c43dfb57573619a58d6d
Denis Zagvozdin <zagvozdeen@gmail.com>
2026-02-23 13:55:29 +0500

    feat(tma): implement register page flow

--- web/shared/src/composables/useFetch.ts
+++ web/shared/src/composables/useFetch.ts
@@ -15,11 +15,10 @@ import type {
   Review,
   Service,
   User, ValidationError,
-} from '../types'
+} from '@shared/types'
 import { type Notify, useNotifications } from '@shared/composables/useNotifications'
 
 type ApiResult<T> = { ok: true; data: T } | { ok: false, data: ValidationError }
-type OnError = (text: string) => void
 
 const getJsonHeaders = (headers?: HeadersInit): Headers => {
   const result = new Headers(headers)
@@ -43,9 +42,6 @@ const fetchJson = async <T>(state: State, notify: Notify, input: RequestInfo, in
   try {
     res = await fetch(input, init)
   } catch {
-    // if (onError !== undefined) {
-    //   onError('Network error')
-    // }
     return {
       ok: false,
       data: {
@@ -57,27 +53,28 @@ const fetchJson = async <T>(state: State, notify: Notify, input: RequestInfo, in
 
   if (!res.ok) {
     if (res.status === 401) {
-      // if (!state.isTelegramEnv()) {
-      //   state.unsetToken()
-      //   location.reload()
-      // }
+      if (!state.isTelegramEnv()) {
+        state.unsetToken()
+        location.reload()
+      }
     }
+
     if (res.headers.get('Content-Type') === 'application/json') {
       return {
         ok: false,
         data: await res.json(),
       }
     }
-    const text = (await res.text()).trim()
 
-    notify.error(i18n[text] || text)
-    // if (onError !== undefined) {
-    //   onError()
-    // }
+    const rawText = (await res.text()).trim()
+    const text=  i18n[rawText] || rawText
+
+    notify.error(text)
+
     return {
       ok: false,
       data: {
-        message: i18n[text] || text,
+        message: text,
         errors: {},
       },
     }
@@ -94,92 +91,92 @@ const login = async (state: State, notify: Notify, payload: object) => {
   })
 }
 
-const register = async (state: State, payload: AuthRegisterRequest, onError?: OnError) => {
-  return fetchJson<AuthRegisterResponse>(state, `${state.getApiUrl()}/api/auth/register`, {
+const register = async (state: State, notify: Notify, payload: AuthRegisterRequest) => {
+  return fetchJson<AuthRegisterResponse>(state, notify, `${state.getApiUrl()}/api/auth/register`, {
     method: 'POST',
     headers: getJsonHeaders(),
     body: JSON.stringify(payload),
-  }, onError)
+  })
 }
 
-const createGuestFeedback = async (state: State, payload: CreateGuestFeedbackRequest, onError?: OnError) => {
-  return fetchJson<Feedback>(state, `${state.getApiUrl()}/api/guest/feedback`, {
+const getMe = async (state: State, notify: Notify) => {
+  return fetchJson<User>(state, notify, `${state.getApiUrl()}/api/me`, {
+    headers: getAuthHeaders(state),
+  })
+}
+
+const createGuestFeedback = async (state: State, notify: Notify, payload: CreateGuestFeedbackRequest) => {
+  return fetchJson<Feedback>(state, notify, `${state.getApiUrl()}/api/guest/feedback`, {
     method: 'POST',
     headers: getJsonHeaders(),
     body: JSON.stringify(payload),
-  }, onError)
+  })
 }
 
-const createGuestOrder = async (state: State, payload: CreateGuestOrderRequest, onError?: OnError) => {
-  return fetchJson<Order>(state, `${state.getApiUrl()}/api/guest/orders`, {
+const createGuestOrder = async (state: State, notify: Notify, payload: CreateGuestOrderRequest) => {
+  return fetchJson<Order>(state, notify, `${state.getApiUrl()}/api/guest/orders`, {
     method: 'POST',
     headers: getJsonHeaders(),
     body: JSON.stringify(payload),
-  }, onError)
+  })
 }
 
-const getMe = async (state: State, onError?: OnError) => {
-  return fetchJson<User>(state, `${state.getApiUrl()}/api/me`, {
+const getProducts = async (state: State, notify: Notify) => {
+  return fetchJson<Product[]>(state, notify, `${state.getApiUrl()}/api/products`, {
     headers: getAuthHeaders(state),
-  }, onError)
+  })
 }
 
-const getProducts = async (state: State, onError?: OnError) => {
-  return fetchJson<Product[]>(state, `${state.getApiUrl()}/api/products`, {
+const getServices = async (state: State, notify: Notify) => {
+  return fetchJson<Service[]>(state, notify, `${state.getApiUrl()}/api/services`, {
     headers: getAuthHeaders(state),
-  }, onError)
+  })
 }
 
-const getServices = async (state: State, onError?: OnError) => {
-  return fetchJson<Service[]>(state, `${state.getApiUrl()}/api/services`, {
+const getCategories = async (state: State, notify: Notify) => {
+  return fetchJson<Category[]>(state, notify, `${state.getApiUrl()}/api/categories`, {
     headers: getAuthHeaders(state),
-  }, onError)
+  })
 }
 
-const getCategories = async (state: State, onError?: OnError) => {
-  return fetchJson<Category[]>(state, `${state.getApiUrl()}/api/categories`, {
+const getFeedback = async (state: State, notify: Notify) => {
+  return fetchJson<Feedback[]>(state, notify, `${state.getApiUrl()}/api/feedback`, {
     headers: getAuthHeaders(state),
-  }, onError)
+  })
 }
 
-const getFeedback = async (state: State, onError?: OnError) => {
-  return fetchJson<Feedback[]>(state, `${state.getApiUrl()}/api/feedback`, {
-    headers: getAuthHeaders(state),
-  }, onError)
-}
-
-const createFeedback = async (state: State, payload: CreateFeedbackRequest, onError?: OnError) => {
-  return fetchJson<Feedback>(state, `${state.getApiUrl()}/api/feedback`, {
+const createFeedback = async (state: State, notify: Notify, payload: CreateFeedbackRequest) => {
+  return fetchJson<Feedback>(state, notify, `${state.getApiUrl()}/api/feedback`, {
     method: 'POST',
     headers: getAuthJsonHeaders(state),
     body: JSON.stringify(payload),
-  }, onError)
+  })
 }
 
-const getReviews = async (state: State, onError?: OnError) => {
-  return fetchJson<Review[]>(state, `${state.getApiUrl()}/api/reviews`, {
+const getReviews = async (state: State, notify: Notify) => {
+  return fetchJson<Review[]>(state, notify, `${state.getApiUrl()}/api/reviews`, {
     headers: getAuthHeaders(state),
-  }, onError)
+  })
 }
 
-const getOrders = async (state: State, onError?: OnError) => {
-  return fetchJson<Order[]>(state, `${state.getApiUrl()}/api/orders`, {
+const getOrders = async (state: State, notify: Notify) => {
+  return fetchJson<Order[]>(state, notify, `${state.getApiUrl()}/api/orders`, {
     headers: getAuthHeaders(state),
-  }, onError)
+  })
 }
 
-const createOrder = async (state: State, payload: CreateOrderRequest, onError?: OnError) => {
-  return fetchJson<Order>(state, `${state.getApiUrl()}/api/orders`, {
+const createOrder = async (state: State, notify: Notify, payload: CreateOrderRequest) => {
+  return fetchJson<Order>(state, notify, `${state.getApiUrl()}/api/orders`, {
     method: 'POST',
     headers: getAuthJsonHeaders(state),
     body: JSON.stringify(payload),
-  }, onError)
+  })
 }
 
-const getUsers = async (state: State, onError?: OnError) => {
-  return fetchJson<User[]>(state, `${state.getApiUrl()}/api/users`, {
+const getUsers = async (state: State, notify: Notify) => {
+  return fetchJson<User[]>(state, notify, `${state.getApiUrl()}/api/users`, {
     headers: getAuthHeaders(state),
-  }, onError)
+  })
 }
 
 export const useFetch = () => {
@@ -188,18 +185,18 @@ export const useFetch = () => {
 
   return {
     login: (payload: object) => login(state, notify, payload),
-    register: (payload: AuthRegisterRequest, onError?: OnError) => register(state, payload, onError),
-    createGuestFeedback: (payload: CreateGuestFeedbackRequest, onError?: OnError) => createGuestFeedback(state, payload, onError),
-    createGuestOrder: (payload: CreateGuestOrderRequest, onError?: OnError) => createGuestOrder(state, payload, onError),
-    getMe: (onError?: OnError) => getMe(state, onError),
-    getProducts: (onError?: OnError) => getProducts(state, onError),
-    getServices: (onError?: OnError) => getServices(state, onError),
-    getCategories: (onError?: OnError) => getCategories(state, onError),
-    getFeedback: (onError?: OnError) => getFeedback(state, onError),
-    createFeedback: (payload: CreateFeedbackRequest, onError?: OnError) => createFeedback(state, payload, onError),
-    getReviews: (onError?: OnError) => getReviews(state, onError),
-    getOrders: (onError?: OnError) => getOrders(state, onError),
-    createOrder: (payload: CreateOrderRequest, onError?: OnError) => createOrder(state, payload, onError),
-    getUsers: (onError?: OnError) => getUsers(state, onError),
+    register: (payload: AuthRegisterRequest) => register(state, notify, payload),
+    getMe: () => getMe(state, notify),
+    createGuestFeedback: (payload: CreateGuestFeedbackRequest) => createGuestFeedback(state, notify, payload),
+    createGuestOrder: (payload: CreateGuestOrderRequest) => createGuestOrder(state, notify, payload),
+    getProducts: () => getProducts(state, notify),
+    getServices: () => getServices(state, notify),
+    getCategories: () => getCategories(state, notify),
+    getFeedback: () => getFeedback(state, notify),
+    createFeedback: (payload: CreateFeedbackRequest) => createFeedback(state, notify, payload),
+    getReviews: () => getReviews(state, notify),
+    getOrders: () => getOrders(state, notify),
+    createOrder: (payload: CreateOrderRequest ) => createOrder(state, notify, payload),
+    getUsers: () => getUsers(state, notify),
   }
 }
---
+++ web/spa/tma/src/pages/RegisterPage.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="min-h-dvh w-full flex items-center justify-center">
+    <n-form
+      ref="formRef"
+      class="w-full bg-gray-500/20 p-4 my-8 rounded-2xl"
+      :rules="rules"
+      :model="form"
+      @submit.prevent="onSubmitForm"
+    >
+      <n-form-item
+        :show-feedback="false"
+        :show-label="false"
+      >
+        <h1 class="text-xl mb-4 text-center w-full">
+          Создать аккаунт
+        </h1>
+      </n-form-item>
+      <n-form-item
+        label="Имя"
+        path="first_name"
+      >
+        <n-input
+          v-model:value="form.first_name"
+          placeholder="Введите имя"
+          autofocus
+        />
+      </n-form-item>
+      <n-form-item
+        label="Фамилия"
+        path="last_name"
+      >
+        <n-input
+          v-model:value="form.last_name"
+          placeholder="Введите фамилию"
+        />
+      </n-form-item>
+      <n-form-item
+        label="Электронная почта"
+        path="email"
+      >
+        <n-input
+          v-model:value="form.email"
+          placeholder="Введите электронную почту"
+        />
+      </n-form-item>
+      <n-form-item
+        label="Пароль"
+        path="password"
+      >
+        <n-input
+          v-model:value="form.password"
+          type="password"
+          placeholder="Введите пароль"
+        />
+      </n-form-item>
+      <n-form-item
+        label="Подтвердите пароль"
+        path="password_confirmation"
+      >
+        <n-input
+          v-model:value="form.password_confirmation"
+          type="password"
+          placeholder="Повторите пароль"
+        />
+      </n-form-item>
+      <n-form-item
+        :show-feedback="false"
+        :show-label="false"
+      >
+        <n-button
+          attr-type="submit"
+          type="success"
+          class="flex-1"
+        >
+          Зарегистрироваться
+        </n-button>
+      </n-form-item>
+
+
+      <p class="text-center mt-4">
+        Уже есть аккаунт? <router-link
+          :to="{ name: 'login' }"
+          class="underline"
+        >
+          Войти в аккаунт
+        </router-link>
+      </p>
+    </n-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { reactive, useTemplateRef } from 'vue'
+import { useFetch } from '@shared/composables/useFetch'
+import type { AuthRegisterRequest } from '@shared/types'
+import { type FormRules, type FormInst, NForm, NFormItem, NButton, NInput } from 'naive-ui'
+import { useSender } from '@shared/composables/useSender'
+import { useNotifications } from '@shared/composables/useNotifications'
+
+const router = useRouter()
+const fetcher = useFetch()
+const sender = useSender()
+const notify = useNotifications()
+const formRef = useTemplateRef<FormInst>('formRef')
+const form = reactive<AuthRegisterRequest>({
+  first_name: null,
+  last_name: null,
+  email: null,
+  password: null,
+  password_confirmation: null,
+})
+const rules: FormRules = {
+  first_name: {
+    required: true,
+    type: 'string',
+    message: 'Введите имя',
+    min: 1,
+    max: 255,
+  },
+  last_name: {
+    required: true,
+    type: 'string',
+    message: 'Введите фамилию',
+    min: 1,
+    max: 255,
+  },
+  email: {
+    required: true,
+    type: 'email',
+    message: 'Введите электронную почту',
+    min: 1,
+    max: 256,
+  },
+  password: {
+    required: true,
+    type: 'string',
+    message: 'Введите пароль от аккаунта',
+    min: 8,
+    max: 72,
+  },
+  password_confirmation: {
+    required: true,
+    type: 'string',
+    message: 'Подтвердите пароль',
+    min: 8,
+    max: 72,
+    validator: (_rule, value: string) => {
+      if (!value) {
+        return new Error('Введите подтверждение пароля')
+      }
+      if (value !== form.password) {
+        return new Error('Пароли не совпадают')
+      }
+      return true
+    },
+  },
+}
+
+const onSubmitForm = () => {
+  sender.submit(formRef.value, async () => {
+    const data = await fetcher.register(form)
+
+    if (data.ok) {
+      notify.info('Аккаунт создан, теперь войдите в систему!')
+      await router.push({ name: 'login' })
+      return
+    }
+
+    notify.error(data.data.message)
+  })
+}
+</script>
